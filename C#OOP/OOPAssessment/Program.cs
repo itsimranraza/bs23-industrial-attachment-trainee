67d839cda1bc39c705f2e63443e8f21c8892cdb7
RAZA <imranraza975@gmail.com>
2026-05-20 19:43:57 +0600

    Expected Code Structure (Skeleton Template)

---
+++ C#OOP/OOPAssessment/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace FareEngineAssessment
+{
+    public enum TripStatus { Pending, Paid, Failed }
+
+    public interface IPromotion
+    {
+        decimal ApplyDiscount(decimal cuurentFare);
+    }
+
+    public interface IPaymentService
+    {
+        bool ProcessPayment(string passengerId, decimal amount);
+    }
+
+    public abstract class Vehicle
+    {
+        
+    }
+
+    public class  Trip
+    {
+        
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
