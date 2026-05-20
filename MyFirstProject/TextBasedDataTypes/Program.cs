f3820c72a3cdf5b3e8aa6d2ebb936cbf3c6e9d93
RAZA <imranraza975@gmail.com>
2026-05-20 15:11:48 +0600

    Text Based Data Types Learned

---
+++ MyFirstProject/TextBasedDataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TextBasedDataTypes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Aba";
+            char letter = 'a';
+
+            Console.Write("Your name is ");
+            Console.Write(name);
+
+            Console.WriteLine();
+            Console.WriteLine(letter);
+
+
+            Console.ReadLine();
+        }
+    }
+}
